fc1741c200b0f1b2cf81aa0213d7fc277aa94e0e
borysigorevich <borysigorevich@gmail.com>
2021-11-18 14:56:30 +0200

    revision 18.11.21

--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -24,28 +24,28 @@ type mockStoreType = {
 }
 
 export const mockStore = (state: mockStateType): PropsType => ({
-    jokesState: {
-        jokes: state.jokes || [],
-        sortedJokes: state.sortedJokes || [],
-        categories: state.categories || [],
-        category: 'all',
-        initialRender: state.initialRender || true,
-        joke: state.joke || '',
-        isLoading: state.isLoading || true,
-        error: {message: state.message || ''}
-    },
-    dispatch: state.dispatch || ((action: actionsType) => {
-        console.log(action)
-    })
-})
+	jokesState: {
+		jokes: state.jokes || [],
+		sortedJokes: state.sortedJokes || [],
+		categories: state.categories || [],
+		category: 'all',
+		initialRender: state.initialRender || true,
+		joke: state.joke || '',
+		isLoading: state.isLoading || true,
+		error: {message: state.message || ''}
+	},
+	dispatch: state.dispatch || ((action: actionsType) => {
+		console.log(action);
+	})
+});
 
 export const mockState = (state: mockStoreType) => ({
-    jokes: state.jokes || [],
-    sortedJokes: state.sortedJokes || [],
-    categories: state.categories || [],
-    category: 'all',
-    initialRender: state.initialRender || true,
-    joke: state.joke || '',
-    isLoading: state.isLoading || true,
-    error: state.error || {message: ''}
-})
+	jokes: state.jokes || [],
+	sortedJokes: state.sortedJokes || [],
+	categories: state.categories || [],
+	category: 'all',
+	initialRender: state.initialRender || true,
+	joke: state.joke || '',
+	isLoading: state.isLoading || true,
+	error: state.error || {message: ''}
+});
